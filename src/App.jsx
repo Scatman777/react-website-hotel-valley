38c01bc7a955b340adfe10de7ac461ad17259494
hostr777 <divakovbogdan11@gmail.com>
2024-03-05 22:24:37 +0300

    Update

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+export default function App() {
+  return (
+	<div>
+		<Header />
+		<main>
+		</main>
+		<Footer />
+	</div>
+  )
+}
