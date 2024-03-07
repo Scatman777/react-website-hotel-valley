6a325e55f9becdb9d9d5fd65d144da1bdc7eca22
hostr777 <divakovbogdan11@gmail.com>
2024-03-07 09:49:13 +0300

    Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,26 @@
 export default function Header() {
 	return (
-		<header>
-
+		<header className="header">
+			<div className="header__couintainer _countainer">
+				<div className="header__body">
+					<nav className="header__navigation">
+						<ul className="header__list">
+							<div className="header__li">
+								<a href="#"></a>
+							</div>
+							<div className="header__li">
+								<a href="#"></a>
+							</div>
+							<div className="header__li">
+								<a href="#"></a>
+							</div>
+							<div className="header__li">
+								<a href="#"></a>
+							</div>
+						</ul>
+					</nav>
+				</div>
+			</div>
 		</header>
 	)
 }
