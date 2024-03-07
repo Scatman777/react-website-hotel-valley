e7621a9f6a656e6e559a7c0a17e750f201e67419
hostr777 <divakovbogdan11@gmail.com>
2024-03-07 11:06:48 +0300

    Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,32 @@
+import '/css/Header-style/App.css'
+import HeaderLogo1 from '/header-img/header-logo1.svg'
+import HeaderLogo2 from '/header-img/header-logo2.svg'
+
 export default function Header() {
 	return (
 		<header className="header">
 			<div className="header__couintainer _countainer">
 				<div className="header__body">
+					<div className="header__column-logo">
+						<img src={HeaderLogo1} alt="headerLogo1" />
+						<img src={HeaderLogo2} alt="headerLogo2" />
+					</div>
 					<nav className="header__navigation">
 						<ul className="header__list">
 							<div className="header__li">
-								<a href="#"></a>
+								<a href="#">Главная</a>
 							</div>
 							<div className="header__li">
-								<a href="#"></a>
+								<a href="#">Номера</a>
 							</div>
 							<div className="header__li">
-								<a href="#"></a>
+								<a href="#">Развлечения</a>
 							</div>
 							<div className="header__li">
-								<a href="#"></a>
+								<a href="#">Трансфер</a>
+							</div>
+							<div className="header__li">
+								<a href="#">Контакты</a>
 							</div>
 						</ul>
 					</nav>
